9768b8024ea44bce0156eede8286e718bda4fb98
kolesnikovaolga <kolesnikovaolga@debian.debian.knu>
2017-12-18 21:25:54 +0200

    lab2.2.11.1

---
+++ swap.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <functional>
+
+using namespace std;
+
+int main() {
+
+	set <double, greater<int>> valuesA = { -1.1, 2.9, -2.3, 3.7 };
+	set <double, greater<int>> valuesB = { -3.14,-2.19,2.71 ,3.88 };
+
+	valuesA.swap(valuesB);
+
+	for (auto value : valuesA)
+		cout << value << " ";
+	cout << endl;
+	for (auto value : valuesB)
+		cout << value << " ";
+	cout << endl;
+
+	return 0;
+}
